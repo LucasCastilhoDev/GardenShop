5ecdc08c00b78a3c6fea023db2cc2ab2576e7112
Lucas Castilho <lucasxcastilho@gmail.com>
2025-04-27 15:59:27 -0300

    adicionado anima scroll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import MenuMobile from "./modules/menu-mobile.js";
+import ScrollAnima from "./modules/scrollAnima.js";
 
 const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']");
 menuMobile.init();
+
+const scrollAnima = new ScrollAnima("[data-anime='scroll']");
+scrollAnima.init();
